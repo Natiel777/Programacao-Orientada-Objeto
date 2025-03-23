abdfe008a4afc45535d8046cff139b4c0f2cdc14
Natiel777 <natiel.silva11@aluno.ifce.edu.br>
2025-03-23 20:15:16 -0300

    Update funcoes.py

--- Unidade 01/Cadastro de Alunos e Notas/funcoes.py
+++ Unidade 01/Cadastro de Alunos e Notas/funcoes.py
@@ -1,1 +1,34 @@
+def adicionar_aluno(alunos, nome, notas):
+    if nome not in alunos:
+        alunos[nome] = notas
+        print(f"Aluno {nome} adicionado com sucesso.")
+    else:
+       print("Aluno já cadastrado.")
+       
+def remover_aluno(alunos, nome):
+    if nome in alunos:
+        alunos[nome] = 0
+        print(f"Aluno {nome} removido com sucesso.")
+    else:
+        print("Aluno não encontrado.")
 
+def atualizar_notas(alunos, nome, notas):
+    if nome in alunos:
+        alunos[nome] = notas
+        print(f"Notas do aluno {nome} atualizadas com sucesso.")
+    else:
+        print("Aluno não encontrado.")
+
+def mostrar_alunos(alunos):
+    if not alunos:
+        print("Nenhum aluno cadastrado.")
+    else:
+        for nome, notas in alunos.items():
+            print(f"{nome}: {notas}")
+
+def calcular_media(alunos, nome):
+    if nome in alunos:
+        media = sum(alunos[nome]) / len(alunos[nome])
+        print(f"Média do aluno {nome}: {media:.2f}")
+    else:
+        print("Aluno não encontrado.")
