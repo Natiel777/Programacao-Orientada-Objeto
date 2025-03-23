eb76acc8fced29dc078f2fa7b9e6a01526f82541
Natiel777 <natiel.silva11@aluno.ifce.edu.br>
2025-03-23 19:46:05 -0300

    Update main.py

--- Unidade 01/Estoque de Produtos/main.py
+++ Unidade 01/Estoque de Produtos/main.py
@@ -1,1 +1,30 @@
+from funcoes import adicionar_produto, remover_produto, atualizar_quantidade, exibir_estoque
 
+while True:
+    print("\n- Sistema de Gerenciamento de Estoque da Loja -")
+    print("1. Adicionar produto")
+    print("2. Remover produto")
+    print("3. Atualizar quantidade de um produto")
+    print("4. Mostrar estoque")
+    print("5. Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        nome = input("Nome do produto: ")
+        quantidade = int(input("Quantidade do produto: "))
+        adicionar_produto(nome, quantidade)
+    elif opcao == "2":
+        nome = input("Nome do produto a ser removido: ")
+        remover_produto(nome)
+    elif opcao == "3":
+        nome = input("Nome do produto: ")
+        quantidade = int(input("Nova quantidade: "))
+        atualizar_quantidade(nome, quantidade)
+    elif opcao == "4":
+        exibir_estoque()
+    elif opcao == "5":
+        print("Saindo do sistema...")
+        break
+    else:
+        print("Opção inválida. Tente novamente outra.")
