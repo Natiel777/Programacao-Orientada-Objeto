c1f19b8747c93d5396fb6095f6188f323b71cc8e
Natiel777 <natiel.silva11@aluno.ifce.edu.br>
2025-04-13 18:32:23 -0300

    Create conta.py

---
+++ Unidade 04/Atividade 01/conta.py
@@ -0,0 +1,53 @@
+from datetime import datetime as dt
+from usuario import Usuario  
+import random
+
+class Conta:   
+    def __init__(self, usuario, data_abertura=dt(1900, 1, 1), saldo=0.0):
+        self.agencia = random.randint(0, 999)
+        self.usuario = usuario
+        self.data_abertura = data_abertura
+        self.saldo = saldo
+	
+    def get_agencia(self):
+        return self.agencia
+    
+    def get_usuario(self):
+        return self.usuario
+    
+    def set_usuario(self, usuario):
+        self.usuario = usuario
+        
+    def get_data_abertura(self):
+        return self.data_abertura
+    
+    def set_data_abertura(self, dia, mes, ano):
+        self.data_abertura = dt(ano, mes, dia)
+        
+    def get_saldo(self):
+        return self.saldo
+    
+    def set_saldo(self, saldo):
+        self.saldo = saldo
+    
+    def consultarSaldo(self):
+        return self.saldo
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+
+    def transferir(self, outra_conta, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+            outra_conta.saldo += valor
+            
+    def __str__(self):
+        return (f"Agência: {self.agencia}\n"
+                f"Usuário: {self.usuario}\n"
+                f"Data de abertura: {self.data_abertura.strftime('%d/%m/%Y')}\n"
+                f"Saldo: R${self.saldo:.2f}")
+
