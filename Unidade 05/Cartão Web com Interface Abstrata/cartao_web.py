79124e9d97821e9e59d89f4fd406d0db5503a944
Natiel777 <natiel.silva11@aluno.ifce.edu.br>
2025-04-20 16:40:06 -0300

    Create cartao_web.py

---
+++ Unidade 05/Cartão Web com Interface Abstrata/cartao_web.py
@@ -0,0 +1,28 @@
+from abc import ABC,abstractmethod
+
+class CartaoWeb(ABC):
+	def __init__(self, destinatario="ind"):
+		self.destinatario = destinatario
+
+	@abstractmethod
+	def show_message(self):
+		pass	
+
+class DiaDosNamorados(CartaoWeb):
+	def __init__(self, destinatario="ind"):	super().__init__(destinatario)
+	
+	def show_message(self):
+		print(f"Feliz Dia dos Namorados! {self.destinatario}")
+	
+class Natal(CartaoWeb):
+	def __init__(self, destinatario="ind"):	super().__init__(destinatario)
+	
+	def show_message(self):
+		print(f"Feliz Natal! {self.destinatario}")
+	
+class Aniversario(CartaoWeb):
+	def __init__(self, destinatario="ind"):	super().__init__(destinatario)
+	
+	def show_message(self):
+		print(f"Feliz Aniversário! {self.destinatario}")
+		
