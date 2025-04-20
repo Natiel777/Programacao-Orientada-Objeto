011c59c943efff1ea8827618bb8829bb667726b5
Natiel777 <natiel.silva11@aluno.ifce.edu.br>
2025-04-20 16:42:52 -0300

    Update main.py

--- Unidade 05/Cartão Web com Interface Abstrata/main.py
+++ Unidade 05/Cartão Web com Interface Abstrata/main.py
@@ -1,1 +1,15 @@
+from cartao_web import DiaDosNamorados, Natal, Aniversario
+		
+cartao1 = DiaDosNamorados("Clare")
+cartao2 = Natal("Natiel")
+cartao3 = Aniversario("Messi")
 
+cartao1.show_message()
+cartao2.show_message()
+cartao3.show_message()
+
+
+"""Polimorfismo acontece no código quando a variável cartao, que é do tipo CartaoWeb, 
+é usada para armazenar objetos de diferentes subclasses (DiaDosNamorados, Natal e Aniversario).
+Mesmo usando o mesmo método show_message(), o comportamento muda conforme a classe do objeto,
+exibindo mensagens diferentes para cada tipo de cartão."""
